40428c187e3941f6b95eb9b571185d4e12ccb806
Your Name <you@example.com>
2017-04-12 15:32:09 -0400

    Address Position Solution

--- workshop.js
+++ workshop.js
@@ -1,3 +1,4 @@
+
 var request = require('request-promise');
 
 // Euclidian distance between two points
@@ -6,7 +7,6 @@ function getDistance(pos1, pos2) {
 }
 
 function getIssPosition() {
-<<<<<<< HEAD
     return request("http://api.open-notify.org/iss-now.json")
         .then(
             function(response) {
@@ -21,76 +21,29 @@ function getIssPosition() {
         }
     )
 }
-
-function getAddressPosition(address) {
-
-}
-
-function getCurrentTemperatureAtPosition(position) {
-
-}
-
-function getCurrentTemperature(address) {
-
-}
-
-function getDistanceFromIss(address) {
-
-}
-
-=======
-    return request('http://api.open-notify.org/iss-now.json')
-        .then(
-            function(res) {
-                res=JSON.parse(res);
-                return {
-                    lat: res.iss_position.latitude,
-                    lng: res.iss_position.longitude
-                };
-            }
-        )
-}
-
 function getAddressPosition(address) {
     return request('https://maps.googleapis.com/maps/api/geocode/json?address=' + address)
-        .then(function(response) {
-            var data = JSON.parse(response);
+    .then(function(response) {
+        var data = JSON.parse(response);
 
-            return data.results[0].geometry.location;
-        });
-}
-
-function getCurrentTemperatureAtPosition(position) {
-    return request('https://api.darksky.net/forecast/e85d4bafe6a41173ae34cf5614a395c9/' + position.lat + ',' + position.lng)
-        .then(function(response) {
-            var data = JSON.parse(response);
-
-            return data.currently.temperature;
-        });
-}
-
-function getCurrentTemperature(address) {
-    return getAddressPosition(address)
-        .then(getCurrentTemperatureAtPosition);
-}
-
-function getDistanceFromIss(address) {
-    return Promise.all([
-        getAddressPosition(address),
-        getIssPosition()
-    ]).then(function(results) {
-        return getDistance(results[0], results[1]);
+        return data.results[0].geometry.location;
     });
 }
 
-exports.getDistance = getDistance;
->>>>>>> 4a84585b278ac643c0c5eef3fa1e79adc1109d2d
+function getCurrentTemperatureAtPosition(position) {
+
+}
+
+function getCurrentTemperature(address) {
+
+}
+
+function getDistanceFromIss(address) {
+
+}
+
 exports.getIssPosition = getIssPosition;
 exports.getAddressPosition = getAddressPosition;
 exports.getCurrentTemperatureAtPosition = getCurrentTemperatureAtPosition;
 exports.getCurrentTemperature = getCurrentTemperature;
-<<<<<<< HEAD
 exports.getDistanceFromIss = getDistanceFromIss;
-=======
-exports.getDistanceFromIss = getDistanceFromIss;
->>>>>>> 4a84585b278ac643c0c5eef3fa1e79adc1109d2d
